9bb40c757b13bebe200e249ac0a24b4f5b69586f
Adam Jacox <yellowjacox@gmail.com>
2018-02-26 11:54:11 -0700

    Heath checkup plug

--- test/health_checkup_test.exs
+++ test/health_checkup_test.exs
@@ -1,8 +1,32 @@
 defmodule HealthCheckupTest do
   use ExUnit.Case
-  doctest HealthCheckup
+  use Plug.Test
 
-  test "greets the world" do
-    assert HealthCheckup.hello() == :world
+  test "it should return a 200 success response with default resp_body" do
+    # Create a test connection
+    opts = HealthCheckup.init([])
+    conn = conn(:get, "/health")
+
+    # Invoke the plug
+    conn = HealthCheckup.call(conn, opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Server's Up!"
+  end
+
+  test "it should return a custom response body" do
+    # Create a test connection
+    opts = HealthCheckup.init([resp_body: "Testing 123"])
+    conn = conn(:get, "/health")
+
+    # Invoke the plug
+    conn = HealthCheckup.call(conn, opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Testing 123"
   end
 end
